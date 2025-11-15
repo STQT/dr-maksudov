df2c3046cbfa6d72ce20c33ba702e2fcdeff41a5
STQT <axmedova@de.ahost.cloud>
2025-11-15 13:58:41 +0500

    fix passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,26 +1,15 @@
 import os
 import sys
-from pathlib import Path
-from typing import Final
 
-# === НАСТРОЙТЕ ПОД СВОЙ ХОСТИНГ =========================================
-# Укажите имя каталога проекта и виртуального окружения на сервере cPanel.
-# Пример: /home/USERNAME/maksudov.uz
-PROJECT_ROOT: Final[Path] = Path.home() / "maksudov.uz"
-VENV_ROOT: Final[Path] = Path.home() / "virtualenv" / "website" / "3.12"
-# =========================================================================
+# ЗАМЕНИТЕ username НА ВАШЕ ИМЯ ПОЛЬЗОВАТЕЛЯ
+INTERP = os.path.expanduser("~/virtualenv/maksudov/3.12/bin/python3.12_bin")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
 
-PYTHON_INTERP = VENV_ROOT / "bin" / "python3.12"
+sys.path.insert(0, os.path.expanduser('~/maksudov'))
+sys.path.insert(0, os.path.expanduser('~/virtualenv/maksudov/3.12/lib/python3.12/site-packages'))
 
-if sys.executable != str(PYTHON_INTERP):
-    os.execl(str(PYTHON_INTERP), str(PYTHON_INTERP), *sys.argv)
-
-sys.path.insert(0, str(PROJECT_ROOT))
-sys.path.insert(0, str(VENV_ROOT / "lib" / "python3.12" / "site-packages"))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maksudov_project.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'maksudov_project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
-
